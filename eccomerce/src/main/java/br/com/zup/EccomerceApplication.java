e2441d20c9dc5b17803fe5cb255dd0d6c4eae3e3
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-29 11:40:51 -0300

    Criar classe produtoDTO

---
+++ eccomerce/src/main/java/br/com/zup/EccomerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceApplication.class, args);
+	}
+
+}
---
+++ eccomerce/src/main/java/br/com/zup/lead/ProdutoDTO.java
@@ -0,0 +1,30 @@
+package br.com.zup.lead;
+
+public class ProdutoDTO {
+    private String nomeProduto;
+    private double valor;
+
+    public ProdutoDTO() {
+    }
+
+    public ProdutoDTO(String nomeProduto, double valor) {
+        this.nomeProduto = nomeProduto;
+        this.valor = valor;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
