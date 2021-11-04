2de4ce1893a5c2470b5b4230f5d4c84cbeed965e
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-11-04 14:05:41 -0300

    Criar lista de investimentos

--- investimentos/src/main/java/br/com/zup/investimentos/investimento/InvestimentoController.java
+++ investimentos/src/main/java/br/com/zup/investimentos/investimento/InvestimentoController.java
@@ -1,8 +1,28 @@
 package br.com.zup.investimentos.investimento;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.investimentos.dtos.InvestimentoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/investimentos")
 public class InvestimentoController {
+
+    @Autowired
+    private InvestimentoService investimentoService;
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarInvestimento (@RequestBody InvestimentoDTO novoInvestimento){
+        investimentoService.salvarInvestimento(novoInvestimento);
+    }
+
+    @GetMapping
+    public List<InvestimentoDTO> exibirInvestimento (){
+        return investimentoService.retornarInvestimentos();
+    }
 
 }
--- investimentos/src/main/java/br/com/zup/investimentos/investimento/InvestimentoService.java
+++ investimentos/src/main/java/br/com/zup/investimentos/investimento/InvestimentoService.java
@@ -1,8 +1,22 @@
 package br.com.zup.investimentos.investimento;
 
+import br.com.zup.investimentos.dtos.InvestimentoDTO;
+import br.com.zup.investimentos.dtos.MontanteDTO;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class InvestimentoService {
+    private List<InvestimentoDTO> investimentos = new ArrayList<>();
+    private List<MontanteDTO> montantes  =new ArrayList<>();
+
+    public void salvarInvestimento (InvestimentoDTO novoInvestimento){
+        investimentos.add(novoInvestimento);
+    }
+
+    public List<InvestimentoDTO> retornarInvestimentos(){
+        return investimentos;
+    }
 
 }
